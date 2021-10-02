2ade8315149edbd2090332f144649b79982d061f
ayush893 <mauryaayush893@gmail.com>
2021-10-02 10:02:02 +0530

    Finished the debugging

--- src/Map.js
+++ src/Map.js
@@ -1,27 +1,48 @@
-import React from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import React from "react";
+import { Map as LeafletMap, TileLayer } from "react-leaflet";
 import "./Map.css";
-import {showDataOnMap} from "./util";
+import { showDataOnMap } from "./util";
 
-function Map({ countries, casesType, center, zoom}) {
-    console.log("dfvfvhbig",center)
-    console.log("zooooooooomm",zoom)
-    return (
-        <div className="map">
-            <MapContainer
-            center={center}
-            zoom={zoom}
-            
-             scrollWheelZoom={false}>
-                <TileLayer
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-
-                {showDataOnMap(countries, casesType)}
-            </MapContainer>
-        </div>
-    )
+function Map({ countries, casesType, center, zoom }) {
+  return (
+    <div className="map">
+      <LeafletMap center={center} zoom={zoom}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {showDataOnMap(countries, casesType)}
+      </LeafletMap>
+    </div>
+  );
 }
 
-export default Map
+export default Map;
+
+// import React from 'react';
+// import { MapContainer, TileLayer } from 'react-leaflet';
+// import "./Map.css";
+// import {showDataOnMap} from "./util";
+
+// function Map({ countries, casesType, center, zoom}) {
+//     console.log("dfvfvhbig",center)
+//     console.log("zooooooooomm",zoom)
+//     return (
+//         <div className="map">
+//             <MapContainer
+//             center={center}
+//             zoom={zoom}
+            
+//              scrollWheelZoom={false}>
+//                 <TileLayer
+//                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+//                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//                 />
+
+//                 {showDataOnMap(countries, casesType)}
+//             </MapContainer>
+//         </div>
+//     )
+// }
+
+// export default Map
